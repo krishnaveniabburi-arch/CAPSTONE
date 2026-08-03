16cef2d99c7d4bfab50593166e1bd66232dd38bf
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-08-03 14:38:18 -0400

    add modified files

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,15 @@
-import 'dotenv/config';
-
-import express from 'express';
+import dotenv from "dotenv";
+import express from "express";
 import cors from 'cors';
-
 import connectDB from './db.js';
 import mongoose from 'mongoose';
 
-import patientroutes from './routes/patientroutes.js';
-import errorHandler  from './middleware/errorHandler.js';
+dotenv.config();
 
 const app = express();
 
 // use production port OR if in development 3000 by default 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 // allow requests from frontend
 app.use(cors());
@@ -20,14 +17,14 @@ app.use(cors());
 // format incoming data to json
 app.use(express.json());
 
-
-
 // start the server
-app.listen(port, () => {
-    console.log('Listening on port: ', port);
-    connectDB();
-})
+app.listen(PORT, () => {
+    console.log("Server started at http://localhost:" + PORT);
+});
 
-export default errorHandler;
+connectDB();
+
+
+
 
 
---
+++ Backend/middleware/errorHandler.js
@@ -0,0 +1,7 @@
+const errorHandler = (err, req, res, next) => {
+    res.json (result);
+}
+
+
+
+export default errorHandler;
