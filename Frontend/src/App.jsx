b5cb434235e87cac7b7ca6e1d3e03a41570f1843
krishnaveniabburi-arch <krishnaveni.abburi@gmail.com>
2026-08-05 14:45:40 -0400

    add modified files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Dashboard from "./views/Dashboard";
 import AddPatient from "./views/AddPatient";
